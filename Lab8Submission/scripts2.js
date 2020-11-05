af302850fefcee7b654bff465dea68ba3822e552
lb17sy <lb17sy@brocku.ca>
2020-11-05 16:30:27 -0500

    Lab 8 completed

---
+++ Lab8Submission/scripts2.js
@@ -0,0 +1,12 @@
+//A function to change colours of elements when called
+function mix(){
+
+	var first = "#BB9320";
+	var second = "#00BB00";
+	var third = "#AB00FF";
+
+	document.getElementById("blue").style.color=first;
+	document.getElementById("green").style.color=second;
+	document.getElementById("red").style.color=third;
+
+}
